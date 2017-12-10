afa98217239be48c6009af3cce51701d84b63dc1
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2017-12-10 23:37:39 +0100

    Update add-year.s

--- addressBook/add-year.s
+++ addressBook/add-year.s
@@ -24,24 +24,24 @@ _start:
 	subl $8, %esp			# allocate space for local variables
 
 	# Open file for reading
-	movl $SYS_OPEN, %eax				# prepare syscall 5 
-	movl $input_file_name, %ebx			# move input file name into %ebx
-	movl $0, %ecx						# open in read-only mode
+	movl $SYS_OPEN, %eax			# prepare syscall 5 
+	movl $input_file_name, %ebx		# move input file name into %ebx
+	movl $0, %ecx				# open in read-only mode
 	movl $0666, %edx		
-	int $LINUX_SYSCALL					# execute system call,
-										# sys_open returns file descriptor in %eax
+	int $LINUX_SYSCALL			# execute system call,
+						# sys_open returns file descriptor in %eax
 
-	movl %eax, ST_INPUT_DESCRIPTOR(%ebp)	 # store the input file descriptor away
+	movl %eax, ST_INPUT_DESCRIPTOR(%ebp)	# store the input file descriptor away
 
 	# Open file for writing
-	movl $SYS_OPEN, %eax				# prepare syscall 5 
+	movl $SYS_OPEN, %eax			# prepare syscall 5 
 	movl $output_file_name, %ebx		# move output file name into %ebx
-	movl $0101, %ecx 					# open in write-only mode
-	movl $0666, %edx					# and create if doesn’t exist
-	int $LINUX_SYSCALL					# execute system call,
-										# sys_open returns file descriptor in %eax
+	movl $0101, %ecx 			# open in write-only mode
+	movl $0666, %edx			# and create if doesn’t exist
+	int $LINUX_SYSCALL			# execute system call,
+						# sys_open returns file descriptor in %eax
 
-	movl %eax, ST_OUTPUT_DESCRIPTOR(%ebp)	 # store the output file descriptor away
+	movl %eax, ST_OUTPUT_DESCRIPTOR(%ebp)	# store the output file descriptor away
 
 loop_begin:
 	pushl ST_INPUT_DESCRIPTOR(%ebp)
@@ -51,7 +51,7 @@ loop_begin:
 
 	# Returns the number of bytes read
 	cmpl $RECORD_SIZE, %eax			# if different from requested number,
-	jne loop_end					# it’s EOF or error, so quit
+	jne loop_end				# it’s EOF or error, so quit
 
 	incl record_buffer + RECORD_AGE		# increment the age
 
@@ -67,5 +67,5 @@ loop_end:
 	movl $SYS_EXIT, %eax
 	movl $0, %ebx
 	int $LINUX_SYSCALL			# execute system call,
-								# sys_exit doesn’t return anything in %eax
+						# sys_exit doesn’t return anything in %eax
 	
