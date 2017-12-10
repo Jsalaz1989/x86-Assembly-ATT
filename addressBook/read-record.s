48ddcdd2035c3eb7ffe1af215674f0ac61d49663
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2017-12-10 23:39:31 +0100

    Update read-record.s

--- addressBook/read-record.s
+++ addressBook/read-record.s
@@ -10,21 +10,21 @@
 	.globl read_record
 	.type read_record, @function
 read_record:
-	pushl %ebp		 	# save old base pointer
-	movl %esp, %ebp		# make stack pointer the base pointer
+	pushl %ebp		 			# save old base pointer
+	movl %esp, %ebp				# make stack pointer the base pointer
 
 	pushl %ebx
 	movl $SYS_READ, %eax
 	movl ST_FILEDES(%ebp), %ebx
 	movl ST_READ_BUFFER(%ebp), %ecx
 	movl $RECORD_SIZE, %edx
-	int $LINUX_SYSCALL		# execute system call,
-							# sys_read returns number of bytes read in %eax,
-							# which is returned to the calling program
+	int $LINUX_SYSCALL				# execute system call,
+									# sys_read returns number of bytes read in %eax,
+									# which is returned to the calling program
 
 
-popl %ebx
+	popl %ebx
 
-movl %ebp, %esp		# restore stack pointer
-popl %ebp			# restore base pointer
-ret					# pop %eip
+	movl %ebp, %esp		# restore stack pointer
+	popl %ebp			# restore base pointer
+	ret					# pop %eip
