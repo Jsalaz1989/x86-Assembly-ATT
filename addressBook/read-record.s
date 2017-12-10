cd0fc658444ab4cb65de5ccb3f9282cfbe811299
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2017-12-10 23:38:52 +0100

    Update read-record.s

--- addressBook/read-record.s
+++ addressBook/read-record.s
@@ -1,30 +1,30 @@
-.include "linux.s"
-.include "record-def.s"
+	.include "linux.s"
+	.include "record-def.s"
 
-#STACK LOCAL VARIABLES
-.equ ST_READ_BUFFER, 8
-.equ ST_FILEDES, 12
+	#STACK LOCAL VARIABLES
+	.equ ST_READ_BUFFER, 8
+	.equ ST_FILEDES, 12
 
-.section .text
+	.section .text
 
-.globl read_record
-.type read_record, @function
+	.globl read_record
+	.type read_record, @function
 read_record:
-pushl %ebp		 	# save old base pointer
-movl %esp, %ebp		# make stack pointer the base pointer
+	pushl %ebp		 	# save old base pointer
+	movl %esp, %ebp		# make stack pointer the base pointer
 
-pushl %ebx
-movl $SYS_READ, %eax
-movl ST_FILEDES(%ebp), %ebx
-movl ST_READ_BUFFER(%ebp), %ecx
-movl $RECORD_SIZE, %edx
-int $LINUX_SYSCALL		# execute system call,
-				# sys_read returns number of bytes read in %eax,
-				# which is returned to the calling program
+	pushl %ebx
+	movl $SYS_READ, %eax
+	movl ST_FILEDES(%ebp), %ebx
+	movl ST_READ_BUFFER(%ebp), %ecx
+	movl $RECORD_SIZE, %edx
+	int $LINUX_SYSCALL		# execute system call,
+							# sys_read returns number of bytes read in %eax,
+							# which is returned to the calling program
 
 
 popl %ebx
 
 movl %ebp, %esp		# restore stack pointer
 popl %ebp			# restore base pointer
-ret				# pop %eip
+ret					# pop %eip
