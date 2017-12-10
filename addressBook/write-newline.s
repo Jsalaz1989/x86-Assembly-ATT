fe84275d5f57acb0a4b9df55aa7d0aa1b2d1024b
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2017-12-10 23:46:30 +0100

    Update write-newline.s

--- addressBook/write-newline.s
+++ addressBook/write-newline.s
@@ -1,25 +1,24 @@
-.include "linux.s"
-.globl write_newline
-.type write_newline, @function
-.section .data
+	.include "linux.s"
+	.globl write_newline
+	.type write_newline, @function
+	.section .data
 
 newline:
-.ascii "\n"
-.section .text
-.equ ST_FILEDES, 8
+	.ascii "\n"
+	.section .text
+	.equ ST_FILEDES, 8
 
 write_newline:
-pushl %ebp
-movl %esp, %ebp
+	pushl %ebp
+	movl %esp, %ebp
 
-movl $SYS_WRITE, %eax
-movl ST_FILEDES(%ebp), %ebx
-movl $newline, %ecx
-movl $1, %edx
-int $LINUX_SYSCALL		# execute system call,
-				# sys_write returns # bytes written in %eax
+	movl $SYS_WRITE, %eax
+	movl ST_FILEDES(%ebp), %ebx
+	movl $newline, %ecx
+	movl $1, %edx
+	int $LINUX_SYSCALL		# execute system call,
+							# sys_write returns # bytes written in %eax
 
-movl %ebp, %esp
-popl %ebp			# restore base pointer
-ret				# pop %eip
-
+	movl %ebp, %esp
+	popl %ebp			# restore base pointer
+	ret					# pop %eip
