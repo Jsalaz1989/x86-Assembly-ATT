56d7e3180b21ff14cd67e307144982ecedf074f4
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2017-12-18 13:58:37 +0100

    Update printf-example.s

--- sharing/printf-example.s
+++ sharing/printf-example.s
@@ -7,14 +7,14 @@ name:
 personstring:
     .ascii "person\0"
 numberloved:
-    .long 3	# could have been an .equ, but decided to give it memory location	
+    .long 3	        # could have been an .equ, but decided to give it memory location	
 
     .section .text
     .globl _start
 _start:
     # Parameters are passed in reverse order than listed in the function’s prototype
     pushl numberloved 		    # this is the %d
-    pushl $personstring 	# this is the second %s
+    pushl $personstring 	    # this is the second %s
     pushl $name 		    # this is the first %s
     pushl $firststring 		    # this is the format string in the prototype
 
