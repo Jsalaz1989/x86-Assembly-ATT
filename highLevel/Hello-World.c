d3fdd1d0a0b0f95dadc5ed8010a10f10957cd32e
Jaime Salazar Lahera <jsalaz1989@gmail.com>
2018-02-11 20:07:56 +0100

    Create Hello-World.c

---
+++ highLevel/Hello-World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* PURPOSE: This program is mean to show a basic C program. 
+ * All it does is print "Hello World!" to the screen and exit. 
+ */
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");       // print our string to standard output
+    
+    return 0;                       // exit with status 0
+}
